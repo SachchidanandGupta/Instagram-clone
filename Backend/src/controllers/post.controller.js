9f619495d062d34519f2a331aab0ba2feaae2cf8
Sachchidanand Gupta <sachchidanandgupta2006@gmail.com>
2026-02-21 15:49:54 +0530

    post creation api imagekit

--- Backend/src/controllers/post.controller.js
+++ Backend/src/controllers/post.controller.js
@@ -18,11 +18,31 @@ const imageKit = new ImageKit({
 async function createPostController(req,res){
    
     console.log(req.body,req.file);
+    
+    const token = req.cookies.token;
+
+    if(!token){
+        return res.status(401).json({
+            message:"Token not provided, unauthorized access."
+        })
+    }
+
+    const decoded = jwt.verify(token,process.env.JWT_SECRET);
+
     const file = await imageKit.files.upload({
         file:await toFile(Buffer.from(req.file.buffer),'file'),
         fileName:"test"
     })
-    res.send(file);
+    console.log(decoded);
+    const post = await postModel.create({
+        caption:req.body.caption,
+        imgUrl:file.url,
+        user:decoded.id
+    });
+    res.status(201).json({
+        message:"Post created successfully",
+        post
+    })
 }
 module.exports ={
     createPostController
