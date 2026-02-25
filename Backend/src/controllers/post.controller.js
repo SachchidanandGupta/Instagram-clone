5b2ebb3b140360c6d9513bd369854f4ff196f64a
Sachchidanand Gupta <sachchidanandgupta2006@gmail.com>
2026-02-25 09:55:40 +0530

    Middleware folder and code

--- Backend/src/controllers/post.controller.js
+++ Backend/src/controllers/post.controller.js
@@ -7,6 +7,7 @@ const bcrypt = require("bcryptjs");
 const ImageKit = require("@imagekit/nodejs");
 
 const { toFile } = require("@imagekit/nodejs");
+
 const userModel = require("../models/user.model");
 
 const imageKit = new ImageKit({
@@ -16,34 +17,19 @@ const imageKit = new ImageKit({
 //multer uses two types of storage disk and memory storage the disk storage means the files are being saved locally(on server) so we use memory storage most of the time which stores data in RAM and is temporary
 
 async function createPostController(req, res) {
-  console.log(req.body, req.file);
+  
 
-  const token = req.cookies.token;
-
-  if (!token) {
-    return res.status(401).json({
-      message: "Token not provided, unauthorized access.",
-    });
-  }
-  let decoded = null;
-  try {
-    decoded = jwt.verify(token, process.env.JWT_SECRET);
-  } catch (err) {
-    return res.status(401).json({
-      message: "user not found UnAuthourized access. ",
-    });
-  }
 
   const file = await imageKit.files.upload({
     file: await toFile(Buffer.from(req.file.buffer), "file"),
     fileName: "test",
     folder: "Cohort-2-Insta-posts",
   });
-  console.log(decoded);
+  
   const post = await postModel.create({
     caption: req.body.caption,
     imgUrl: file.url,
-    user: decoded.id,
+    user: req.user.id,
   });
   res.status(201).json({
     message: "Post created successfully",
@@ -52,22 +38,9 @@ async function createPostController(req, res) {
 }
 
 async function getPostController(req, res) {
-  const token = req.cookies.token;
-  if (!token) {
-    return res.status(401).json({
-      message: "Unauthorized access.",
-    });
-  }
-  let decoded = null;
-  try {
-    decoded = jwt.verify(token, process.env.JWT_SECRET);
-  } catch (err) {
-    return res.status(401).json({
-      message: " not an Authorized user. ",
-    });
-  }
+ 
 
-  const userId = decoded.id;
+  const userId = req.user.id;
   const posts = await postModel.find({
     user: userId,
   });
@@ -79,45 +52,31 @@ async function getPostController(req, res) {
 }
 
 async function getPostDetailsController(req, res) {
-  const token = req.cookies.token;
-  if (!token) {
-    return res.status(401).json({
-      message: "Unauthorized access.",
-    });
-  }
-  let decoded = null;
-  try{
-    decoded = jwt.verify(token,process.env.JWT_SECRET);
-  }catch(err){
-    return res.status(401).json({
-      message:"Invalid token! "
-    })
-  }
+ 
 
-  const userId = decoded.id;
+  const userId = req.user.id;
 
   const postId = req.params.postId;
 
   const posts = await postModel.findById(postId);
-  if(!posts){
+  if (!posts) {
     return res.status(404).json({
-      message:"Post  not founded."
-    })
+      message: "Post  not founded.",
+    });
   }
 
   const isValidUser = posts.user.toString() === userId;
 
-  if(!isValidUser){
+  if (!isValidUser) {
     return res.status(403).json({
-      message:"Forbidden content."
-    })
-  };
+      message: "Forbidden content.",
+    });
+  }
 
   return res.status(200).json({
-    message:"Post fetched successfully.",
-    posts
-  })
-
+    message: "Post fetched successfully.",
+    posts,
+  });
 }
 
 module.exports = {
--- Backend/src/routes/post.routes.js
+++ Backend/src/routes/post.routes.js
@@ -4,14 +4,16 @@ const postRouter = express.Router();
 
 const postController = require("../controllers/post.controller");
 
+const identifyUser = require("../middlewares/auth.middleware");
+
 const multer = require("multer");
 
 const upload = multer({storage:multer.memoryStorage()});
 
-postRouter.post("/",upload.single("image"),postController.createPostController);
+postRouter.post("/",upload.single("image"),identifyUser,postController.createPostController);
 
-postRouter.get("/",postController.getPostController);
+postRouter.get("/",identifyUser,postController.getPostController);
 
-postRouter.get("/details/:postId",postController.getPostDetailsController);
+postRouter.get("/details/:postId",identifyUser,postController.getPostDetailsController);
 
 module.exports = postRouter;
