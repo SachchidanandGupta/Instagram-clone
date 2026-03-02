d659048116652429614c0789e4882ada8b1e5f4f
Sachchidanand Gupta <sachchidanandgupta2006@gmail.com>
2026-03-02 16:15:21 +0530

    Frontend setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { RouterProvider } from "react-router";
+import AppRoutes from "./Approutes";
+
+const App = () => {
+  return (
+    <div>
+       <AppRoutes/>
+    </div>
+  )
+}
+
+export default App
